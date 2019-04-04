ca2bd5dc0e8ce117fcb2f29a3d3245397aec9cc7
DiamondDagger590 <diamonddagger590@gmail.com>
2019-04-03 19:35:39 -0500

    Added skill tables

---
+++ src/main/java/us/eunoians/mcrpg/database/tables/ArcheryTable.java
@@ -0,0 +1,32 @@
+package us.eunoians.mcrpg.database.tables;
+
+import com.cyr1en.flatdb.annotations.Column;
+import com.cyr1en.flatdb.annotations.Table;
+
+@Table(nameOverride = "archery_data")
+public class ArcheryTable {
+
+  @Column
+  private int current_exp;
+  @Column private int level;
+
+  @Column private boolean is_daze_toggled;
+  @Column private boolean is_puncture_toggled;
+  @Column private boolean is_tipped_arrows_toggled;
+  @Column private boolean is_combo_toggled;
+  @Column private boolean is_blessing_of_artemis_toggled;
+  @Column private boolean is_blessing_of_apollo_toggled;
+  @Column private boolean is_curse_of_hades_toggled;
+
+
+  @Column private boolean puncture_tier;
+  @Column private boolean tipped_arrows_tier;
+  @Column private boolean combo_tier;
+  @Column private boolean blessing_of_artemis_tier;
+  @Column private boolean blessing_of_apollo_tier;
+  @Column private boolean curse_of_hades_tier;
+
+  @Column private long blessing_of_artemis_cooldowns;
+  @Column private long blessing_of_apollo_cooldowns;
+  @Column private long curse_of_hades_cooldowns;
+}
---
+++ src/main/java/us/eunoians/mcrpg/database/tables/HerbalismTable.java
@@ -0,0 +1,31 @@
+package us.eunoians.mcrpg.database.tables;
+
+import com.cyr1en.flatdb.annotations.Column;
+import com.cyr1en.flatdb.annotations.Table;
+
+@Table(nameOverride = "herbalism_data")
+public class HerbalismTable {
+
+  @Column
+  private int current_exp;
+  @Column private int level;
+
+  @Column private boolean is_too_many_plants_toggled;
+  @Column private boolean is_farmers_diet_toggled;
+  @Column private boolean is_diamond_flowers_toggled;
+  @Column private boolean is_replanting_toggled;
+  @Column private boolean is_mass_harvest_toggled;
+  @Column private boolean is_natures_wrath_toggled;
+  @Column private boolean is_pans_blessing_toggled;
+
+  @Column private boolean farmers_diet_tier;
+  @Column private boolean diamond_flowers_tier;
+  @Column private boolean replanting_tier;
+  @Column private boolean mass_harvest_tier;
+  @Column private boolean natures_wrath_tier;
+  @Column private boolean pans_blessing_tier;
+
+  @Column private long mass_harvest_cooldowns;
+  @Column private long natures_wrath_cooldowns;
+  @Column private long pans_blessing_cooldowns;
+}
