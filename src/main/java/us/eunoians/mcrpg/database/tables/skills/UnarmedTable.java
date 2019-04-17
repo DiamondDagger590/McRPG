28274e853991703bd5a54e3f3a3125c1d41c6f77
DiamondDagger590 <diamonddagger590@gmail.com>
2019-04-16 23:54:26 -0500

    Idek wtf is wrong at this point

--- src/main/java/us/eunoians/mcrpg/database/tables/skills/UnarmedTable.java
+++ src/main/java/us/eunoians/mcrpg/database/tables/skills/UnarmedTable.java
@@ -9,8 +9,8 @@ public class UnarmedTable {
   @Column(autoIncrement = true) private int id;
   @Column(primaryKey = true) private String uuid;
 
-  @Column private int current_exp;
-  @Column private int level;
+  @Column(defaultValue = "0") private int current_exp;
+  @Column(defaultValue = "0") private int current_level;
 
   @Column(defaultValue = "1") private boolean is_sticky_fingers_toggled;
   @Column(defaultValue = "1") private boolean is_tighter_grip_toggled;
@@ -20,14 +20,21 @@ public class UnarmedTable {
   @Column(defaultValue = "1") private boolean is_smiting_fist_toggled;
   @Column(defaultValue = "1") private boolean is_dense_impact_toggled;
 
-  @Column private int tighter_grip_tier;
-  @Column private int disarm_tier;
-  @Column private int iron_arm_tier;
-  @Column private int berserk_tier;
-  @Column private int smiting_fist_tier;
-  @Column private int dense_impact_tier;
+  @Column(defaultValue = "0") private int tighter_grip_tier;
+  @Column(defaultValue = "0") private int disarm_tier;
+  @Column(defaultValue = "0") private int iron_arm_tier;
+  @Column(defaultValue = "0") private int berserk_tier;
+  @Column(defaultValue = "0") private int smiting_fist_tier;
+  @Column(defaultValue = "0") private int dense_impact_tier;
 
-  @Column private long berserk_cooldown;
-  @Column private long smiting_fist_cooldown;
-  @Column private long dense_impact_cooldown;
+  @Column(defaultValue = "0") private int berserk_cooldown;
+  @Column(defaultValue = "0") private int smiting_fist_cooldown;
+  @Column(defaultValue = "0") private int dense_impact_cooldown;
+
+  @Column(defaultValue = "0") private boolean is_tighter_grip_pending;
+  @Column(defaultValue = "0") private boolean is_disarm_pending;
+  @Column(defaultValue = "0") private boolean is_iron_arm_pending;
+  @Column(defaultValue = "0") private boolean is_berserk_pending;
+  @Column(defaultValue = "0") private boolean is_smiting_fist_pending;
+  @Column(defaultValue = "0") private boolean is_dense_impact_pending;
 }
