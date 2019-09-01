b55204336918f8dfc9b9932f7656c3645e1343e0
DiamondDagger590 <diamonddagger590@gmail.com>
2019-09-01 14:34:55 -0500

    Potion Factory

--- src/main/java/us/eunoians/mcrpg/api/util/brewing/BasePotion.java
+++ src/main/java/us/eunoians/mcrpg/api/util/brewing/BasePotion.java
@@ -1,32 +1,79 @@
 package us.eunoians.mcrpg.api.util.brewing;
 
+import de.tr7zw.changeme.nbtapi.NBTItem;
 import lombok.Getter;
+import lombok.Setter;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.PotionMeta;
+import org.bukkit.potion.PotionData;
 import us.eunoians.mcrpg.types.BasePotionType;
 
 import static us.eunoians.mcrpg.api.util.brewing.PotionFactory.getBasePotionTypeFromItemStack;
 
-public class BasePotion{
+public class BasePotion {
 
-	@Getter
-	private boolean isSplash;
+  @Getter @Setter
+  private boolean isSplash;
 
-	@Getter
-	private boolean isLingering;
+  @Getter @Setter
+  private boolean isLingering;
 
-	@Getter
-	private BasePotionType basePotionType;
+  @Getter
+  private BasePotionType basePotionType;
 
-	@Getter
-	private ItemStack potionItem;
+  @Getter
+  private ItemStack potionItem;
 
-	BasePotion(ItemStack potion){
-		this.basePotionType = getBasePotionTypeFromItemStack(potion);
-		this.isSplash = potion.getType() == Material.SPLASH_POTION;
-		this.isLingering = potion.getType() == Material.LINGERING_POTION;
-		this.potionItem = potion;
-	}
+  @Getter
+  private int timesExtended = 0;
+  @Getter @Setter
+  private int currentExtendedAmount = 0;
+  @Getter
+  private int timesAmplified = 0;
+  @Getter @Setter
+  private int currentAmplifiedAmount = 0;
+  @Getter
+  private int totalTimesModified = 0;
+  private NBTItem nbtItem;
 
+  BasePotion(ItemStack potion){
+    this.basePotionType = getBasePotionTypeFromItemStack(potion);
+    this.isSplash = potion.getType() == Material.SPLASH_POTION;
+    this.isLingering = potion.getType() == Material.LINGERING_POTION;
+    this.potionItem = potion;
+    nbtItem = new NBTItem(potionItem);
+    PotionMeta meta = (PotionMeta) potion.getItemMeta();
+    PotionData data = meta != null ? meta.getBasePotionData() : null;
+    if(nbtItem.hasNBTData()){
+      currentAmplifiedAmount = nbtItem.hasKey("CurrentAmplified") ? nbtItem.getInteger("CurrentAmplified") : data.isUpgraded() ? 1 : 0;
+      currentExtendedAmount = nbtItem.hasKey("CurrentExtended") ? nbtItem.getInteger("CurrentExtended") : data.isExtended() ? 1 : 0;
+      timesAmplified = nbtItem.hasKey("TimesAmplified") ? nbtItem.getInteger("TimesAmplified") : currentAmplifiedAmount;
+      timesExtended = nbtItem.hasKey("TimesExtended") ? nbtItem.getInteger("TimesExtended") : currentExtendedAmount;
+      totalTimesModified = nbtItem.hasKey("TotalModified") ? nbtItem.getInteger("TotalModified") : timesAmplified + timesExtended;
+      potionItem = nbtItem.getItem();
+    }
+    else{
+      if(data != null){
+        timesAmplified = data.isUpgraded() ? 1 : 0;
+        timesExtended = data.isExtended() ? 1 : 0;
+        currentAmplifiedAmount = timesAmplified;
+        currentExtendedAmount = timesExtended;
+        totalTimesModified = timesAmplified + timesExtended;
+      }
+    }
+    saveStack();
+  }
 
+  public void saveStack(){
+    nbtItem.setInteger("CurrentAmplified", currentAmplifiedAmount);
+    nbtItem.setInteger("CurrentExtended", currentExtendedAmount);
+    nbtItem.setInteger("TimesAmplified", timesAmplified);
+    nbtItem.setInteger("TimesExtended", timesExtended);
+    nbtItem.setInteger("TotalModified", totalTimesModified);
+  }
+
+  public ItemStack getAsItem(){
+    return nbtItem.getItem();
+  }
 }
--- src/main/java/us/eunoians/mcrpg/api/util/brewing/PotionFactory.java
+++ src/main/java/us/eunoians/mcrpg/api/util/brewing/PotionFactory.java
@@ -7,33 +7,32 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import us.eunoians.mcrpg.types.BasePotionType;
 
-public class PotionFactory{
+public class PotionFactory {
 
-	static BasePotion convertItemStackToBasePotion(ItemStack potion){
-		BasePotion returnPotion = new BasePotion(potion);
-		return returnPotion;
-	}
+  static BasePotion convertItemStackToBasePotion(ItemStack potion){
+    return new BasePotion(potion);
+  }
 
 
-	@Nullable
-	static BasePotionType getBasePotionTypeFromItemStack(@NotNull ItemStack potion){
-		if(!potion.getType().name().contains("POTION")){
-			return null;
-		}
-		else{
-			PotionMeta meta = (PotionMeta) potion.getItemMeta();
-			boolean isCustom = meta.getBasePotionData().getType() == PotionType.AWKWARD;
-			if(!isCustom){
-				return BasePotionType.getFromPotionType(meta.getBasePotionData().getType());
-			}
-			else{
-				if(meta.hasCustomEffects()){
-					return BasePotionType.getFromPotionEffect(meta.getCustomEffects().get(0).getType());
-				}
-				else{
-					return null;
-				}
-			}
-		}
-	}
+  @Nullable
+  static BasePotionType getBasePotionTypeFromItemStack(@NotNull ItemStack potion){
+    if(!potion.getType().name().contains("POTION")){
+      return null;
+    }
+    else{
+      PotionMeta meta = (PotionMeta) potion.getItemMeta();
+      boolean isCustom = meta.getBasePotionData().getType() == PotionType.AWKWARD;
+      if(!isCustom){
+        return BasePotionType.getFromPotionType(meta.getBasePotionData().getType());
+      }
+      else{
+        if(meta.hasCustomEffects()){
+          return BasePotionType.getFromPotionEffect(meta.getCustomEffects().get(0).getType());
+        }
+        else{
+          return null;
+        }
+      }
+    }
+  }
 }
