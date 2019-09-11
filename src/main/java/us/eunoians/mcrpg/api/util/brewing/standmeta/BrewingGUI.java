4715a9c6617a7ae6ed22a434b4dfc3769fdd6327
DiamondDagger590 <diamonddagger590@gmail.com>
2019-09-11 09:29:15 -0500

    Progress on writing save for brewingstand

--- src/main/java/us/eunoians/mcrpg/api/util/brewing/standmeta/BrewingGUI.java
+++ src/main/java/us/eunoians/mcrpg/api/util/brewing/standmeta/BrewingGUI.java
@@ -2,9 +2,18 @@ package us.eunoians.mcrpg.api.util.brewing.standmeta;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.bukkit.Location;
 import org.bukkit.block.BrewingStand;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.inventory.ItemStack;
+import us.eunoians.mcrpg.McRPG;
 import us.eunoians.mcrpg.api.util.brewing.BasePotion;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 public class BrewingGUI {
 
@@ -23,8 +32,50 @@ public class BrewingGUI {
   @Getter @Setter
   private int currentBrewProgress;
 
-  private ItemStack[] specialRewards;
+  private ItemStack[] specialRewards = new ItemStack[3];
 
-  private BasePotion[] potionItems;
-  
+  private BasePotion[] potionItems = new BasePotion[3];
+
+  public List<ItemStack> getSpecialRewards(){
+    return Arrays.asList(specialRewards);
+  }
+
+  public List<BasePotion> getPotionItems(){
+    return Arrays.asList(potionItems);
+  }
+
+  public void save(){
+    File folder = new File(McRPG.getInstance().getDataFolder(), File.separator + "brewing_storage");
+    if(!folder.exists()){
+      folder.mkdirs();
+    }
+    File storageFile = new File(folder, File.separator + convertLocToFileName(holder.getLocation()));
+    if(!storageFile.exists()){
+      try{
+        storageFile.createNewFile();
+      }
+      catch(IOException e){
+        e.printStackTrace();
+      }
+    }
+    FileConfiguration storage = YamlConfiguration.loadConfiguration(storageFile);
+    storage.set("Progress", currentBrewProgress);
+    storage.set("FuelLv", currentFuelLevel);
+    storage.set("Fuel", fuel);
+    storage.set("Ing", ingredient);
+    for(int i = 0; i < specialRewards.length; i++){
+      if(specialRewards[i] != null){
+        storage.set("Spec" + (i+1), specialRewards[i]);
+      }
+    }
+    for(int i = 0; i < potionItems.length; i++){
+      if(specialRewards[i] != null){
+        storage.set("BP" + (i+1), potionItems[i].getAsItem());
+      }
+    }
+  }
+
+  private String convertLocToFileName(Location loc){
+    return loc.getBlockX() + ":" + loc.getBlockY() + ":" + loc.getBlockZ() + ":" + loc.getWorld().getName();
+  }
 }
