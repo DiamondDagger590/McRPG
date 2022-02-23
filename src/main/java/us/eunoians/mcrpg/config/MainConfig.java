762364744675bc53c90167306eaa2115cc18ed90
DiamondDagger590 <diamonddagger590@gmail.com>
2022-02-23 11:11:46 -0600

    Add more to config

--- src/main/java/us/eunoians/mcrpg/config/MainConfig.java
+++ src/main/java/us/eunoians/mcrpg/config/MainConfig.java
@@ -3,6 +3,8 @@ package us.eunoians.mcrpg.config;
 import de.articdive.enum_to_yaml.interfaces.ConfigurationEnum;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Collections;
+
 /**
  * The enum that is used to generate the main config.yml for McRPG
  *
@@ -26,20 +28,53 @@ public enum MainConfig implements ConfigurationEnum {
     MCMMO_CONVERSION_EQUATION("mcmmo.conversion-equation", "((skill_exp)*0.5)",
         "#Converts a players level to exp and then convert that into 'boosted experience' using the equation below",
         "#To configure how boosted exp works, please look at the 'boosted-exp' section"),
-    BOOSTED_EXP_USAGE_RATE("boosted-exp.usage-rate", "((gained_exp)*2.25)",
-        "#When a player gains exp, this equation is factored in and if there is remaining boosted exp,",
-        "#then it will add this equation value to the gained amount"),
 
     //General Configuration
+    DISABLED_WORLDS("configuration.disabled-worlds", Collections.singletonList("test"),
+        "#What worlds should McRPG be disabled in"),
+    SAVE_INTERVAL("configuration.save-interval", 1,
+        "#This is how often the plugin saves player data (async) in minutes"),
     DISABLE_TIPS("configuration.disable-tips", false,
         "#If true, then McRPG gameplay tips will not be sent to any players"),
+    LANGUAGE_FILE("configuration.language-file", "en",
+        "#What lang file you want to use. Do not include the .yml"),
 
     //Exp modifications
     MAX_DAMAGE_CAP("configuration.experience.max-damage-cap-to-award-exp", 1000000,
         "#The maximum amount of damage allowed for giving experience"),
     EXP_MULTIPLIER_CAP("configuration.experience.exp-multiplier-limit", 3.0,
         "#The max amount of exp multiplier that a player should be able to get from various exp modifiers"),
-    SHIELD_BLOCKING_MODIFIER("configuration.ShieldBlockingModifier", 0.5, "#How much exp gain should be modified if the target is blocking with a shield."),
+    SHIELD_BLOCKING_MODIFIER("configuration.experience.shield-blocking-modifier", 0.5,
+        "#How much the experience gain should be modified if the target is blocking with a shield.",
+        "#This is mostly used to lower the ability of players to use shields to farm exp, while still awarding experience"),
+    DISABLE_ENDER_PEARL_EXP("configuration.experience.disable-experience-from-ender-pearls", true,
+        "#If experience gain should be disabled for damage from ender pearls"),
+    BOOSTED_EXP_USAGE_RATE("configuration.experience.boosted-exp.usage-rate", "((gained_exp)*2.25)",
+        "#When a player gains exp, this equation is factored in and if there is remaining boosted exp,",
+        "#then it will add this equation value to the gained amount"),
+
+    //Skill Books
+    DISABLE_BOOKS_IN_END("configuration.skill-books.disable-books-in-end", true,
+        "#If enabled, skill books will be unable to be dropped by blocks and mobs in the end"),
+
+    //Admin
+    ABILITY_SPY_ENABLED("configuration.admin.enable-ability-spy", false,
+        "#If enabled, admins will be alerted when abilities are unlocked and upgraded"),
+
+    //Exp display
+    EXP_UPDATES_ENABLED("configuration.display.exp-updates.enabled", false,
+        "#If players should be sent a display every time they gain experience"),
+    EXP_UPDATES_DISPLAY_TYPE("configuration.display.exp-updates.display-type", "BOSSBAR",
+        "#What type should the display be. Accepted values are: BOSSBAR, SCOREBOARD, or ACTIONBAR"),
+    EXP_UPDATES_DISPLAY_DURATION("configuration.display.exp-updates.display-duration", 3,
+        "#Duration of the reminder in seconds. Only used for scoreboard and bossbar"),
+
+    //Gameplay
+    REPLACE_ABILITY_COOLDOWN("configuration.gameplay.replace-ability-cooldown", 1440,
+        "#How long the cooldown for replacing an ability should be in minutes"),
+    REQUIRE_EMPTY_OFF_HAND("configuration.gameplay-require-empty-off-hand-to-ready", false,
+        "#If enabled, players will be required to have an empty off hand in order to ready their abilities"),
+
     ;
 
     private final String path;
