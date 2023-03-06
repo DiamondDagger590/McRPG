a16e1eec69bab3878e06f894190685642b3e6a2c
DiamondDagger590 <diamonddagger590@gmail.com>
2023-03-05 23:19:38 -0500

    Finally get stupid build working for 1.19 (how did this take me 3 hours??)

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,20 +34,21 @@ java {
 repositories {
     mavenCentral()
     mavenLocal()
-    jcenter()
 
     maven("https://jitpack.io")
     maven("https://repo.extendedclip.com/content/repositories/placeholderapi/") //Papi
-    maven("https://maven.sk89q.com/repo/") //WorldGuard
+    maven("https://maven.enginehub.org/repo/") //WorldGuard
     maven("https://nexus.wesjd.net/repository/thirdparty/")
     maven("https://oss.sonatype.org/content/repositories/snapshots/")
-    maven("https://repo.codemc.org/repository/maven-public/")
+    maven("https://repo.codemc.io/repository/maven-public/")
     maven("https://repo.aikar.co/content/groups/aikar/")
 
     //Spigot
+    maven("https://oss.sonatype.org/content/repositories/snapshots/")
+    maven("https://oss.sonatype.org/content/repositories/central/")
     maven("https://repo.md-5.net/content/repositories/snapshots/")
     maven("https://repo.md-5.net/content/repositories/releases/")
-    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots")
 
 }
 
@@ -56,14 +57,14 @@ dependencies {
     val intellijAnnotationVersion = "12.0"
     implementation("com.intellij:annotations:$intellijAnnotationVersion")
 
-    val spigotVersion = "1.19-R0.1-SNAPSHOT"
+    val spigotVersion = "1.19.3-R0.1-SNAPSHOT"
     compileOnly("org.spigotmc:spigot-api:$spigotVersion")
 
     // TODO Look into new spigot yaml comment api
     val enumToYamlVersion = "1.0"
     implementation("com.github.DiamondDagger590:EnumToYaml:$enumToYamlVersion")
 
-    val nbtAPIVersion = "2.10.0"
+    val nbtAPIVersion = "2.11.2-SNAPSHOT"
     implementation("de.tr7zw:item-nbt-api:$nbtAPIVersion")
 
     val bstatsVersion = "2.2.1"
@@ -72,9 +73,8 @@ dependencies {
     val placeholderAPIVersion = "2.9.2"
     compileOnly("me.clip:placeholderapi:$placeholderAPIVersion")
 
-    val worldGuardVersion = "7.0.0-SNAPSHOT"
-    compileOnly("com.sk89q.worldguard:worldguard-core:$worldGuardVersion")
-    compileOnly("com.sk89q.worldguard:worldguard-legacy:$worldGuardVersion")
+    val worldGuardVersion = "7.0.7"
+    compileOnly("com.sk89q.worldguard:worldguard-bukkit:$worldGuardVersion")
 
     //Jar deps
     compileOnly(files("libs/Sickle.jar"))
