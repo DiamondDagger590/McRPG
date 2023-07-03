364fe9978bcb58ad3aa0528bb03a34f7d6d00175
DiamondDagger590 <diamonddagger590@gmail.com>
2023-07-03 02:13:57 -0400

    <3

---
+++ src/main/java/Sonja.java
@@ -0,0 +1,12 @@
+public class Sonja {
+    private String uwu;
+
+    public Sonja() {
+        uwu = "i miss you";
+    }
+
+    public void print_uwu(){
+        System.out.println(this.uwu);
+    }
+
+}
